41d656b096bb85550e12e0131301bc584efab153
vinod <vinodkumarbatchu123@gmail.com>
2019-06-22 11:51:01 +0530

    Updated

--- Packages/numerical.py
+++ Packages/numerical.py
@@ -9,3 +9,12 @@ def isprime(n):
     if c==1:
         return True
  
+#Function to determine number of Prime factors for a given number
+def numberOfPrimeFactors(n):
+    if isPrime(n):
+        return 1
+    count = 0
+    for i in range(2, n // 2 + 1):
+        if isPrime(i) and n % i == 0:
+            count += 1
+    return count
---
+++ Packages/validators.py
@@ -0,0 +1,23 @@
+# Function to validate a phone number
+import re
+
+def phoneNumberValidator(number):
+    pattern = '^[6-9][0-9]{9}$|^[0][6-9][0-9]{9}|^[+][9][1][6-9][0-9]{9}$'
+    if re.match(pattern, str(number)):
+        return True
+    return False
+
+def emailValidator(email):
+    pattern = "^[0-9a-z][0-9a-z_.]{4,13}[0-9a-z][@][a-z0-9]{3,18}[.][a-z]{2,4}$"
+    if re.match(pattern, email):
+        return True
+    return False
+
+
+import re
+# Function to check if contact already exists
+def checkContactExists(name):
+    with open('DataFiles/contacts.txt', 'r') as f:
+        filedata = f.read()
+        pattern = name+','
+    return re.search(pattern, filedata)
